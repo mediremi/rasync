a350b15c22b1fd417d37481553776f04fdca488c
Médi-Rémi Hashim <mediremi@gmail.com>
2014-12-27 19:42:16 +0000

    Updated require test

--- test/require.js
+++ test/require.js
@@ -2,9 +2,18 @@ var test = require("tape")
 var rasync = require("../")
 
 test("Can require modules", function(t) {
-	t.plan(1)
+	t.plan(2)
 
 	rasync("fs", function(err, fs) {
-		t.assert(fs === require("fs"))
+		t.assert(err !== null, "no errors")
+		t.assert(fs === require("fs"), "module loaded")
+	})
+})
+
+test("Returns error if module cannot be loaded", function(t) {
+	t.plan(1)
+
+	rasync("non-existant-module", function(err) {
+		t.assert(err === null)
 	})
 })
