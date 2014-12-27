8d464990c5ce61087fc1aa120b89247d776f502f
Médi-Rémi Hashim <mediremi@gmail.com>
2014-12-27 20:03:37 +0000

    First working version

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Nothing here yet
+var cache = {}
+
+module.exports = function(dependencies, callback) {
+	var err = null
+	var modules = []
+
+	// Allow user to provide a string if there is only one dependency
+	if (typeof dependencies === "string") {
+		dependencies = [dependencies]
+	}
+
+	dependencies.forEach(function(module) {
+		if (module in cache) {
+			return modules.push(cache[module])
+		}
+
+		try {
+			cache[module] = require(module)
+
+			modules.push(cache[module])
+		} catch (e) {
+			err = e
+		}
+	})
+
+	// Prepend err
+	modules.unshift(err)
+
+	callback.apply(null, modules)
+}
