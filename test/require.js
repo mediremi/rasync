771afa268a1336b9dfb2094c5452df64d72c53a2
Médi-Rémi Hashim <mediremi@gmail.com>
2014-12-27 20:02:07 +0000

    Fixed test

--- test/require.js
+++ test/require.js
@@ -5,7 +5,7 @@ test("Can require modules", function(t) {
 	t.plan(2)
 
 	rasync("fs", function(err, fs) {
-		t.assert(err !== null, "no errors")
+		t.assert(err === null, "no errors")
 		t.assert(fs === require("fs"), "module loaded")
 	})
 })
@@ -14,6 +14,6 @@ test("Returns error if module cannot be loaded", function(t) {
 	t.plan(1)
 
 	rasync("non-existant-module", function(err) {
-		t.assert(err === null)
+		t.assert(err !== null)
 	})
 })
